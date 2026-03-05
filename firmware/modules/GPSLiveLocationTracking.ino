859e8ceba0e5951fd9d7010925aec03ce099af22
S.D.D Silva <sandirudilmika25@gmail.com>
2026-03-05 12:21:38 +0530

    Refactor GPS tracking to use Serial2 and Firebase
    Updated GPS tracking code to use Serial2 for GPS data and added Firebase integration for location data.

--- firmware/modules/GPSLiveLocationTracking.ino
+++ firmware/modules/GPSLiveLocationTracking.ino
@@ -1,41 +1,85 @@
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
 #include <TinyGPS++.h>
-#include <HardwareSerial.h>
+
+// WIFI 
+#define WIFI_SSID "Naveesha's A56"
+#define WIFI_PASSWORD "nb2k6900"
+
+
+#define API_KEY "AIzaSyAu7nrubc0mqtKsmA22mgLLpQkYAkAEy74"
+#define DATABASE_URL "livestock-monitoring-sys-ad970-default-rtdb.asia-southeast1.firebasedatabase.app"
+
+
+#define RXD2 16
+#define TXD2 17
 
 TinyGPSPlus gps;
 
-// Use UART2 on ESP32
-HardwareSerial gpsSerial(2);
+// Firebase objects
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
 
 void setup() {
-  Serial.begin(115200);
-  gpsSerial.begin(9600, SERIAL_8N1, 16, 17); // RX=16, TX=17
 
-  Serial.println("ESP32 GPS Tracker Started...");
+  Serial.begin(115200);
+  delay(1000);
+
+  // Start GPS serial
+  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+
+  // WiFi Connection
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("\nWiFi Connected");
+
+  // Firebase configuration
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+  config.signer.test_mode = true;
+
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+
+  Serial.println("Firebase Ready");
+  Serial.println("GPS Module Ready");
 }
 
 void loop() {
-  while (gpsSerial.available() > 0) {
-    gps.encode(gpsSerial.read());
+
+  while (Serial2.available() > 0) {
+
+    char c = Serial2.read();
+    gps.encode(c);
 
     if (gps.location.isUpdated()) {
-      double lat = gps.location.lat();
-      double lng = gps.location.lng();
 
-      Serial.println("----- GPS Data -----");
-      Serial.print("Latitude: "); Serial.println(lat, 6);
-      Serial.print("Longitude: "); Serial.println(lng, 6);
-      Serial.print("Satellites: "); Serial.println(gps.satellites.value());
-      Serial.print("Altitude (m): "); Serial.println(gps.altitude.meters());
-      Serial.print("Speed (km/h): "); Serial.println(gps.speed.kmph());
+      float latitude = gps.location.lat();
+      float longitude = gps.location.lng();
 
-      // Google Maps link
-      Serial.print("Google Maps: ");
-      Serial.print("https://www.google.com/maps?q=");
-      Serial.print(lat, 6);
-      Serial.print(",");
-      Serial.println(lng, 6);
+      Serial.println("---- GPS DATA ----");
+      Serial.print("Latitude: ");
+      Serial.println(latitude, 6);
 
-      Serial.println("--------------------\n");
+      Serial.print("Longitude: ");
+      Serial.println(longitude, 6);
+
+      Serial.println("------------------");
+
+      // Send to Firebase
+      Firebase.RTDB.setFloat(&fbdo, "/livestock/gps/latitude", latitude);
+      Firebase.RTDB.setFloat(&fbdo, "/livestock/gps/longitude", longitude);
+
+      Serial.println("Location Sent To Firebase");
+
     }
   }
+
 }
